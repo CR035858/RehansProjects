b34a32e3dde65deebc4bc7e4e6925962fe96f08d
CR035858 <shekarchandru.5@gmail.com>
2024-02-29 12:28:59 +0530

    Add files via upload

---
+++ springSecurity4a/src/main/java/com/gl/springSecurity/entity/MyUserDetails.java
@@ -0,0 +1,83 @@
+package com.gl.springSecurity.entity;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+//This is myuserdetail instance which is getting live data from user 
+
+public class MyUserDetails implements UserDetails {
+
+	private String userName;
+	private String password;
+	private boolean active;
+	private List<GrantedAuthority> authorities;
+	
+
+	public MyUserDetails() {
+
+	}
+
+	//Now in  constructor i need to extract those value from the user instance that we
+	// got from the repository and convert it into Myuserdetail instance which is what userdetailservice is going to pass
+	//to spring security 
+	public MyUserDetails(User user) {
+		this.userName = user.getUserName();
+		this.password = user.getPassword();
+		this.active = user.isActive();
+		this.authorities= Arrays.stream(user.getRoles().split(","))
+				          .map(SimpleGrantedAuthority::new)
+				          .collect(Collectors.toList());
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+	}
+
+	@Override
+	public String getPassword() {
+
+		return password;
+	}
+
+	
+
+	@Override
+	public boolean isAccountNonExpired() {
+
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+}
---
+++ springSecurity4a/src/main/java/com/gl/springSecurity/entity/User.java
@@ -0,0 +1,62 @@
+package com.gl.springSecurity.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class User{
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String userName;
+	@Column
+	private String password;
+	@Column
+	private boolean active;
+	@Column 
+	private String roles;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+	
+}
+
+
